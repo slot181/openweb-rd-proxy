6214249f568a240cfd56b970c3136d6431102a84
slot181 <58209373+slot181@users.noreply.github.com>
2024-12-28 00:12:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use('/', createProxyMiddleware({
   secure: false, // 禁用 SSL 证书验证,用于自签证书
   changeOrigin: true,
   ws: true, // 支持 WebSocket
+  onProxyRes: (proxyRes, req, res) => {
+    // 添加 Access-Control-Allow-Origin 头
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // 或者你的域名
+    proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+    proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+  },
 }));
 
 app.listen(process.env.PORT || 3000, () => {
